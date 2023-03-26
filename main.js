ba05cdb93423138b3ac8a165fd068dd3e74ac9d8
Lewis Benson <tm.lewis.benson@gmail.com.com>
2023-03-25 19:18:09 -0500

    add analytics npm package

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict';
-analytics('Guess A Number');
+const analyticsScriptTag = window.generateScriptTag(
+  'Guess-A-Number',
+  'd526e49d-cc0f-468f-b04d-f59e21f6365a',
+);
+document.body.innerHTML += analyticsScriptTag;
 
 let gameOn = 1;
 let score = 0;
